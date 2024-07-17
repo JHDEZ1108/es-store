b8b49e55e6288eb08a4a215f48baa726b60541f0
Josué Hernández <joheca1998@outlook.com>
2024-07-17 15:39:29 -0600

    Añadiendo modularidad

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,4 +1,13 @@
 import { Module } from '@nestjs/common';
 
-@Module({})
+import { CustomerController } from '../users/controllers/customers/customers.controller';
+import { CustomersService } from '../users/services/customers/customers.service';
+import { UsersController } from '../users/controllers/users/users.controller';
+import { UsersService } from '../users/services/users/users.service';
+
+@Module({
+  imports: [],
+  controllers: [CustomerController, UsersController],
+  providers: [CustomersService, UsersService],
+})
 export class UsersModule {}
