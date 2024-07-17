3eef486a787e9281ee50ebba19c8ff38f3eda3b3
Josué Hernández <joheca1998@outlook.com>
2024-07-17 15:10:32 -0600

    Creando controladores para brand y category

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,13 @@ import { AppController } from './products/controllers/app/app.controller';
 import { AppService } from './products/services/app/app.service';
 import { UsersModule } from './users/users.module';
 import { ProductsModule } from './products/products.module';
+import { BrandsService } from './brands/brands.service';
+import { CategoriesService } from './categories/categories.service';
+import { BrandsController } from './brands/brands.controller';
 
 @Module({
   imports: [UsersModule, ProductsModule],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, BrandsController],
+  providers: [AppService, BrandsService, CategoriesService],
 })
 export class AppModule {}
