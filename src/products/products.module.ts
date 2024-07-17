84668d99c826f1c1e357f9d65a27c54f43a6e03d
Josué Hernández <joheca1998@outlook.com>
2024-07-17 15:16:02 -0600

    Añadiendo configuracion

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
-import { CategoriesController } from './controllers/categories/categories.controller';
+
 import { ProductsController } from './controllers/products/products.controller';
+import { BrandsController } from './controllers/brands/brands.controller';
+import { CategoriesController } from './controllers/categories/categories.controller';
 import { ProductsService } from './services/products/products.service';
+import { BrandsService } from './services/brands/brands.service';
+import { CategoriesService } from './services/categories/categories.service';
 
 @Module({
-  controllers: [CategoriesController, ProductsController],
-  providers: [ProductsService],
+  controllers: [ProductsController, CategoriesController, BrandsController],
+  providers: [ProductsService, BrandsService, CategoriesService],
 })
 export class ProductsModule {}
